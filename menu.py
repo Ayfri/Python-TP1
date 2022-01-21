95b0086f6e290f913b15af01ae4b74272c779ac7
Ayfri <pierre.ayfri@gmail.com>
2022-01-21 16:50:56 +0100

    refactor: Remove old menu.

--- menu.py
+++ menu.py
@@ -1,117 +1,67 @@
-import queue
-import subprocess
-import sys
-import tkinter as tk
-import threading
-import traceback
-from tkinter import Button
-from tkinter.font import Font
+from typing import Callable
+from utils.prints import Color, print_line
 
+exercises: list[str] = [
+	'Types prédéfinis',
+	'Calcul d’une surface',
+	'Somme',
+	'Factorielle',
+	'Table de multiplications',
+	'Saisie de nombres et de caractères',
+	'Calcul de crédit'
+]
 
-queue = queue.Queue()
+def open_exercice(number: int) -> None:
+	"""
+	Ouvre un exercice en fonction de son numéro.
 
-def openExercice(number: int) -> None:
-	print(f"Opening exercice {number}")
-	module = __import__(f"TP1.ex{number}")
-	subModule = getattr(module, f"ex{number}")
-	function = getattr(subModule, f"tp{number}")
+	:param number: Le numéro de l'exercice à ouvrir.
+	:type number: int
+	:return: None
+	:rtype: None
+	"""
+	module: object = __import__(f"src.ex{number}")
+	sub_module: object = getattr(module, f"ex{number}")
+	function: Callable = getattr(sub_module, f"ex{number}")
 	function()
 
-def generateButtons(frm: tk.Frame) -> None:
-	for i in range(1, 8):
-		button: Btn = Btn(queue, i)
-		button.createButton(frm)
+def print_terminal_menu() -> None:
+	"""
+	Affiche le menu principal du terminal.
 
-class TextRedirector(object):
-	def __init__(self, widget: tk.Text, tag="stdout") -> None:
-		self.widget = widget
-		self.tag = tag
+	:return: None
+	:rtype: None
+	"""
+	exercises_list: str = '\n'.join(f"Exercice {Color.CYAN}{i}{Color.END} : {Color.GREEN}{exercises[i - 1]}{Color.END}" for i in range(1, 8))
+	print_line("Menu", color = Color.YELLOW)
+	print(f"{exercises_list}\n\n{Color.RED}STOP pour arrêter.{Color.END}")
 
-	def readline(self, *string: str, **kargs) -> str:
-		self.widget.configure(state="normal")
-		self.widget.insert("end", *string, self.tag)
-		self.widget.configure(state="disabled")
-		return string
+def main() -> None:
+	"""
+	Fonction principale du programme.
+	Affiche le menu de sélection des exercices.
 
-	def flush(self) -> None:
-		pass
+	:return: None
+	:rtype: None
+	"""
+	print(f"{Color.RED}Bienvenue dans le menu du TP 1{Color.END}")
+	while True:
+		print_terminal_menu()
+		try:
+			input1: str = input(f"{Color.BLUE}Veuillez choisir un exercice à réaliser : {Color.END}")
+			if input1.lower() == "stop":
+				raise KeyboardInterrupt
+			elif input1.isdigit() and int(input1) in range(1, 8):
+				print(f"Lancement de l'exercice n°{input1}")
+				print_line(f"Exercice n°{input1}", color = Color.YELLOW)
+				open_exercice(int(input1))
+				print("\nTapez ENTRER pour revenir dans le menu.")
+				input()
+			else:
+				print(f"{Color.YELLOW}Veuillez entrer un nombre entre 1 et 7.{Color.END}")
+		except Exception:
+			print(f"{Color.CYAN}Au revoir :){Color.END}")
+			exit(1)
 
-	def write(self, string: str) -> None:
-		self.widget.configure(state="normal")
-		self.widget.insert("end", string, self.tag)
-		self.widget.configure(state="disabled")
-
-class Btn(threading.Thread):
-	btn: Button
-	font: Font
-
-	def __init__(self, q, number: int):
-		super().__init__()
-		self.queue = q
-		self.number = number
-		self.worker = True
-		self.font = Font(family="Helvetica", size=13)
-
-	def run(self) -> None:
-		queue.put("")
-		while True:
-			try:
-				self.queue.get()
-				terminalText.enable()
-				sys.stdout = TextRedirector(terminalText.text)
-				def event(event: tk.Event) -> None:
-					if event.char.isascii() and event.char.isprintable():
-						sys.stdout.write(event.char)
-
-				terminalText.text.bind("<Key>", event)
-				terminalText.text.bind("<Delete>", lambda _: sys.stdout.write("\b"))
-				terminalText.text.bind("<BackSpace>", lambda _: sys.stdout.write("\b"))
-				terminalText.text.bind("<Return>", lambda _: sys.stdout.readline())
-				terminalText.text.bind("<Tab>", lambda _: sys.stdout.write("\t"))
-
-				print(terminalText.text.configure(state=tk.NORMAL), terminalText.text["state"])
-				openExercice(self.number)
-			except BaseException as e:
-				traceback.print_exception(e)
-			finally:
-				self.queue.task_done()
-
-	def createButton(self, frm: tk.Frame) -> None:
-		self.btn = Button(frm, text=f"Exercice {self.number}", bg='gray', fg='white', font=self.font, command=self.start)
-		self.btn.configure(height=3, width=15, relief='solid', overrelief='solid', font=self.font)
-		self.btn.grid(row=self.number, column=0)
-
-class TerminalBox:
-	text: tk.Text
-
-	def __init__(self, frm: tk.Frame):
-		self.font = Font(family="Helvetica", size=13)
-
-		self.text = tk.Text(frm, height=100, width=150, font=self.font)
-		self.text.grid(row=0, column=0)
-		self.disable()
-
-	def redirector(self, inputStr: str) -> None:
-		self.text.insert(tk.INSERT, inputStr)
-
-	def enable(self) -> None:
-		self.text.configure(state="normal")
-
-	def disable(self) -> None:
-		self.text.configure(state="disabled")
-
-
-root = tk.Tk()
-root.geometry("1600x900")
-root.tk_setPalette(background='#000000', foreground='#ffffff')
-buttonsFrame = tk.Frame(root, bg='black')
-buttonsFrame.grid(row=0, column=0, sticky="nsew")
-
-terminalFrame = tk.Frame(root, width=1600, bg='black')
-terminalFrame.grid(row=0, column=1, sticky="nsew")
-terminalText = TerminalBox(terminalFrame)
-
-generateButtons(buttonsFrame)
-
-queue.join()
-root.mainloop()
+if __name__ == "__main__":
+	main()
